1d652b1d403308e70fd954b9d014a82e1ba4be34
deepankaranand <deepravi0522@gmail.com>
2020-12-20 23:04:44 +0530

    New java 8

---
+++ src/com/java/RunnableLambdaExample.java
@@ -0,0 +1,27 @@
+package com.java;
+
+public class RunnableLambdaExample {
+    public static void main(String[] args) {
+        /**
+         * Prior Java 8
+         */
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Inside runnable 1");
+            }
+        };
+        new Thread(runnable).start();
+
+        /**
+         * Java 8
+         * () -> {}
+          **/
+        Runnable runnableLambda = () -> {
+            System.out.println("Inside runnable 2");
+        };
+        new Thread(runnableLambda).start();
+
+        new Thread(()-> System.out.println("Inside runnable 3")).start();
+    }
+}
