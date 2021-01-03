7a06b77c9f0e37f54030e7ba0cf7309db093ed65
deepankaranand <deepravi0522@gmail.com>
2021-01-03 22:53:54 +0530

    added predicate student

--- src/com/java/functionalinterface/PredicateStudentExample.java
+++ src/com/java/functionalinterface/PredicateStudentExample.java
@@ -1,2 +1,70 @@
-package com.java.functionalinterface;public class PredicateStudentExample {
+package com.java.functionalinterface;
+
+import com.java.data.Student;
+import com.java.data.StudentDataBase;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateStudentExample {
+  static Predicate<Student> p1 = student -> student.getGradeLevel() >= 3;
+  static Predicate<Student> p2 = student -> student.getGpa() >= 3.9;
+  public static void filterStudentByGradeLevel(){
+    System.out.println("filterStudentByGradeLevel: ");
+    List<Student> studentList = StudentDataBase.getAllStudents();
+    studentList.forEach(
+        student -> {
+          if (p1.test(student)) {
+            System.out.println(student);
+          }
+        });
+    }
+  public static void filterStudentByGpa(){
+    System.out.println("filterStudentByGpa: ");
+    List<Student> studentList = StudentDataBase.getAllStudents();
+    studentList.forEach(
+            student -> {
+              if (p2.test(student)) {
+                System.out.println(student);
+              }
+            });
+  }
+  public static void filterStudentAnd(){
+    System.out.println("filterStudentAnd: ");
+    List<Student> studentList = StudentDataBase.getAllStudents();
+    studentList.forEach(
+            student -> {
+              if (p1.and(p2).test(student)) {
+                System.out.println(student);
+              }
+            });
+  }
+  public static void filterStudentOr(){
+    System.out.println("filterStudentOr: ");
+    List<Student> studentList = StudentDataBase.getAllStudents();
+    studentList.forEach(
+            student -> {
+              if (p1.or(p2).test(student)) {
+                System.out.println(student);
+              }
+            });
+  }
+  public static void filterStudentNegate(){
+    System.out.println("filterStudentNegate: ");
+    List<Student> studentList = StudentDataBase.getAllStudents();
+    studentList.forEach(
+            student -> {
+              if (p1.or(p2).negate().test(student)) {
+                System.out.println(student);
+              }
+            });
+  }
+
+  public static void main(String[] args) {
+    filterStudentByGradeLevel();
+    filterStudentByGpa();
+    filterStudentAnd();
+    filterStudentOr();
+    filterStudentNegate();
+  }
 }
