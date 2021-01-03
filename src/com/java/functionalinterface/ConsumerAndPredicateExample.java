5aa7927d19cca32db8b3bba117355d12aa896bff
deepankaranand <deepravi0522@gmail.com>
2021-01-04 00:10:48 +0530

    added predicate student

--- src/com/java/functionalinterface/ConsumerAndPredicateExample.java
+++ src/com/java/functionalinterface/ConsumerAndPredicateExample.java
@@ -5,6 +5,7 @@ import com.java.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,14 +13,21 @@ public class ConsumerAndPredicateExample {
 
   Predicate<Student> p1 = student -> student.getGradeLevel() >= 3;
   Predicate<Student> p2 = student -> student.getGpa() >= 3.9;
+
+  BiPredicate<Integer,Double> biPredicate = (gradeLevel, gpa) -> gradeLevel>=3 && gpa>=3.9;
+
   BiConsumer<String, List<String>> listBiConsumer =
       (name, activities) -> System.out.println(name + " : " + activities);
 
   Consumer<Student> studentConsumer =
-      student -> {
-        if (p1.and(p2).test(student)) {
-          listBiConsumer.accept(student.getName(), student.getActivities());
-        };
+//      student -> {
+//        if (p1.and(p2).test(student)) {
+//          listBiConsumer.accept(student.getName(), student.getActivities());
+//        };
+          student -> {
+              if (biPredicate.test(student.getGradeLevel(),student.getGpa())) {
+                  listBiConsumer.accept(student.getName(), student.getActivities());
+              };
       };
 
   public void printNameAndActivities(List<Student> studentList) {
